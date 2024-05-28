c4bdd38964a55b0e953841ffce048fe3bbcd832f
Elia Wäfler <158746986+eliawaefler@users.noreply.github.com>
2024-05-28 08:03:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
 # classify
+
+# /classify_KBOB/app.py
+
+from myutils.version_check import check_version
+
+
+def main():
+    check_version()
+    # Your project code here
+
+
+if __name__ == "__main__":
+    main()
